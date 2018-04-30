cd686ba1835c90fbc5b2b0be1eb54c2840e3bec6
DanBlakeNZ <dblakenz@hotmail.comm>
2018-04-30 11:42:21 +1000

    play with import export

--- indicision-app/src/app.js
+++ indicision-app/src/app.js
@@ -1,1 +1,12 @@
-console.log('app.js is running!')
+// import subtract, { square, add } from './utils.js'
+
+// console.log('app.js is running!');
+// console.log(square(4));
+// console.log(add(100, 23));
+// console.log(subtract(100, 20));
+
+import isSenior, {isAdult, canDrink} from "./person";
+
+console.log("Is adult: ", isAdult(18));
+console.log("Can drink: ", canDrink(20));
+console.log("Is senior: ", isSenior(65));
