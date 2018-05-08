0e972d0cfe9ccb24e01aebbcf9f049ce5158524a
DanBlakeNZ <dblakenz@hotmail.com>
2018-05-08 20:20:19 +1000

    Added container class. Created settings syles

--- indicision-app/src/components/IndicisionApp.js
+++ indicision-app/src/components/IndicisionApp.js
@@ -76,18 +76,20 @@ export default class IndicisionApp extends React.Component {
 		return(
 			<div>
 				<Header subtitle={subtitle}/>
-				<Action 
-					hasOptions={this.state.options.length > 0}
-					handlePick={this.handlePick}
-				/>
-				<Options 
-					options={this.state.options}
-					handleDeleteOption={this.handleDeleteOption}
-					handleDeleteOptions={this.handleDeleteOptions}
-				/>
-				<AddOption
-					handleAddOption={this.handleAddOption}
-				/>
+				<div className="container">
+					<Action 
+						hasOptions={this.state.options.length > 0}
+						handlePick={this.handlePick}
+					/>
+					<Options 
+						options={this.state.options}
+						handleDeleteOption={this.handleDeleteOption}
+						handleDeleteOptions={this.handleDeleteOptions}
+					/>
+					<AddOption
+						handleAddOption={this.handleAddOption}
+					/>
+				</div>
 				<OptionModal 
 					selectedOption={this.state.selectedOption}
 					handleClearSelectedOption={this.handleClearSelectedOption}
