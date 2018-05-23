d37dcd67a388def5557930afafacdd187cb306b5
DanBlakeNZ <dblakenz@hotmail.com>
2018-05-23 18:55:01 +1000

    Created navigation links to components

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 
@@ -30,19 +30,32 @@ const HelpPage = ()=> (
 
 const NotFoundPage = ()=> (
     <div>
-       404!
+       404! - <Link to='/'>Go Home</Link>
     </div>
+);
+
+const Header = ()=> (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    </header>
 );
 
 const routes = (
     <BrowserRouter>
-        <Switch> // Like a JS switch this will go through the below routes and look for a match and then stop
-            <Route path="/" component={ExpenseDashboardPage} exact={true} />
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/edit" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage} />
-        </Switch>
+        <div>
+            <Header />
+            <Switch> // Like a JS switch this will go through the below routes and look for a match and then stop
+                <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit" component={EditExpensePage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
     </BrowserRouter>
 )
 
