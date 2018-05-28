d15529eb44b0f6cb17f77456fc0f6dfdfac1a8c2
DanBlakeNZ <dblakenz@hotmail.com>
2018-05-28 19:20:32 +1000

    redux playground - actions

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,7 +1,41 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => { //sets the default state
-    return state;
+const store = createStore((state = { count: 0 }, action) => {
+    switch(action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        case 'RESET':
+            return {
+                count: 0
+            }
+
+        default:
+            return state;
+    }
 });
 
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'DECREMENT'
+})
+store.dispatch({
+    type: 'RESET'
+})
+
+store.dispatch({
+    type: 'DECREMENT'
+})
+
 console.log(store.getState()) // returns the current state object.
+
