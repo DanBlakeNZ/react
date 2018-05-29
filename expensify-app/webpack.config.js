eac34a9a8803287564dea07e012c63defd0dd69b
DanBlakeNZ <dblakenz@hotmail.com>
2018-05-29 19:52:34 +1000

    object destructuring

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/playground/redux-101.js',
+    entry: './src/playground/destructuring.js',
     output: {
         path: path.join(__dirname, 'public'), // This is the absolule path to where you want to output
         filename: 'bundle.js'
