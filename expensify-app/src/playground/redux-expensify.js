625ca3cb3bcd267527e791c0ca4500959c26c68d
DanBlakeNZ <dblakenz@hotmail.com>
2018-05-31 19:05:50 +1000

    setup combine reducers

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,5 +1,55 @@
 import { createStore, combineReducers } from 'redux';
 
+// Expenses Reducer
+
+const expensesReducerDefaultState =[];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+
+        default:
+            return state;
+    }
+
+}
+
+const filtersReducerDefaultState = {
+    text: 'rent',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+}
+
+const filtersReducer = (state = filtersReducerDefaultState, action ) => {
+    switch (action.type){
+
+        default:
+            return state;
+    }
+}
+
+
+// Filters Reducer
+
+
+// Store
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filtersReducer: filtersReducer
+    })
+);
+
+//key is rootstate name, value is the reducer
+console.log(store.getState());
+
+
+
+
+
+
+
 const demoState = {
     expenses: [{
         id: 'adsfasdfa',
