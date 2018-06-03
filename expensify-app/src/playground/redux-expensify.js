90ec6a1fcda26c5045d4fd10e1c66eeb993a2b69
DanBlakeNZ <dblakenz@hotmail.com>
2018-06-03 17:59:25 +1000

    added sorting

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -158,7 +158,15 @@ const getVisableExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;
-    });
+    }).sort((a,b) => {
+        if(sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1;
+        }
+
+        if(sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
+    })
 }
 
 
@@ -182,16 +190,16 @@ store.subscribe( ()=> {
 })
 
 
-const expenseOne = store.dispatch(addExpense({ description:'Rent', amount: 100, createdAt: 1000 }));
+const expenseOne = store.dispatch(addExpense({ description:'Rent', amount: 100, createdAt: -21000 }));
 const expenseTwo = store.dispatch(addExpense({ description:'Coffee', amount: 300, createdAt: -1000 }));
 
 // store.dispatch(removeExpense( {id: expenseOne.expense.id } ));
 // store.dispatch(editExpense( expenseTwo.expense.id, { amount: 500 }));
 
-store.dispatch(setTextFilter("re"));
+// store.dispatch(setTextFilter("re"));
 // store.dispatch(setTextFilter());
 
-// store.dispatch(sortbyAmount());
+store.dispatch(sortbyAmount());
 // store.dispatch(sortbyDate());
 
 // store.dispatch(setStartDate(0));
