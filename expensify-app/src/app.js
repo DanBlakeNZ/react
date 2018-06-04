5933774ad69751e04e45f5d1d1e133d522dd659f
DanBlakeNZ <dblakenz@hotmail.com>
2018-06-04 20:11:43 +1000

    added redux store, actions, reducers to indecsion app

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter'
+import AppRouter from './routers/AppRouter';
+
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisableExpenses from './selectors/expenses'
+
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description:'Water Bill' }));
+store.dispatch(addExpense({ description:'Gas Bill' }));
+store.dispatch(setTextFilter("water"));
+
+const state = store.getState();
+const visableExpenses = getVisableExpenses(state.expenses, state.filters)
+
+console.log(visableExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
