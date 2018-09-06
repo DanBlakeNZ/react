3abb6d4248ea7f1191d2abbbd14b3e3d3146eacf
DanBlakeNZ <dblakenz@hotmail.com>
2018-09-06 15:59:15 +1000

    tests for expenses actions

--- expensify-app/src/actions/expenses.js
+++ expensify-app/src/actions/expenses.js
@@ -1,32 +1,30 @@
 import uuid from 'uuid';
 
-export const addExpense = ( {
-    description = '', 
-    note ='', 
-    amount = 0, 
-    createdAt = 0 
-} = {}
+export const addExpense = (
+	{
+		description = '', 
+		note ='', 
+		amount = 0, 
+		createdAt = 0 
+	} = {}
 ) => ({
-    type: 'ADD_EXPENSE',
-    expense: {
-        id: uuid(),
-        description, //description: description
-        note,
-        amount,
-        createdAt
-    }
+	type: 'ADD_EXPENSE',
+	expense: {
+		id: uuid(),
+		description, //description: description
+		note,
+		amount,
+		createdAt
+	}
 });
 
-export const removeExpense = ( { 
-    id 
-    } = {} 
-    ) => ({
-    type:'REMOVE_EXPENSE',
-    id
+export const removeExpense = ( { id } = {} ) => ({
+	type:'REMOVE_EXPENSE',
+	id
 });
 
 export const editExpense = (id, updates) => ({
-    type: 'EDIT_EXPENSE',
-    id,
-    updates
+	type: 'EDIT_EXPENSE',
+	id,
+	updates
 });
