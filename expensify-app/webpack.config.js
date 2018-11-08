57e35b39e81b4b373807bdec48a5097b39c552a3
DanBlakeNZ <dblakenz@hotmail.com>
2018-11-08 15:01:34 +1100

    Initial Auth login setup. Fixed Typos

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -47,17 +47,17 @@ module.exports = (env) => {
 			}]
 		},
 		plugins: [
-			CSSExtract,
-			new webpack.DefinePlugin({
-				'process.env.FIREBASE_API_key': JSON.stringify(process.env.FIREBASE_API_key),
-				'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-				'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-				'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-				'process.env.FIREBASE_STOREAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STOREAGE_BUCKET),
-				'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+      CSSExtract,
+      new webpack.DefinePlugin({
+				'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
       })
-		],
-		devtool: isProduction ? 'source-map' : 'inline-source-map',
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
 		devServer: {
 			contentBase: path.join(__dirname, 'public'),
 			historyApiFallback: true, // Tells the devserver that we are using clientside routing, and for all 404 routes it should fallback to index.html
