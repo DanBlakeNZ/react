ee173cfdcf079e59e3433d6e21ea5986978a264a
DanBlakeNZ <dblakenz@hotmail.com>
2018-11-08 11:40:43 +1100

    load expenses from firebase

--- expensify-app/src/reducers/expenses.js
+++ expensify-app/src/reducers/expenses.js
@@ -1,32 +1,35 @@
-const expensesReducerDefaultState =[];
+const expensesReducerDefaultState = [];
 
 export default (state = expensesReducerDefaultState, action) => {
-    
-    switch (action.type) {
 
-        case "ADD_EXPENSE":
-            return [
-                ...state,
-                action.expense
-            ]
-        
-        case "REMOVE_EXPENSE":
-            return state.filter(( {id} ) => id !== action.id )
+	switch (action.type) {
 
-        case "EDIT_EXPENSE":
-            return state.map((expense) => {
-                if(expense.id === action.id){
-                    return {
-                        ...expense,
-                        ...action.updates //this overrides any existing values from expense.
-                    }
-                }else{
-                    return expense;
-                }
-            })
+		case "ADD_EXPENSE":
+			return [
+				...state,
+				action.expense
+			]
 
-        default:
-            return state;
-    }
+		case "REMOVE_EXPENSE":
+			return state.filter(({ id }) => id !== action.id)
+
+		case "EDIT_EXPENSE":
+			return state.map((expense) => {
+				if (expense.id === action.id) {
+					return {
+						...expense,
+						...action.updates //this overrides any existing values from expense.
+					}
+				} else {
+					return expense;
+				}
+			})
+
+		case "SET_EXPENSES":
+			return action.expenses
+
+		default:
+			return state;
+	}
 
 }
