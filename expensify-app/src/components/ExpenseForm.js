cd27bda91b6542d0cfe365e0afc7c4c7ed0d4df2
DanBlakeNZ <dblakenz@hotmail.com>
2018-11-16 16:42:40 +1100

    adding app styles

--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -4,97 +4,100 @@ import { SingleDatePicker } from 'react-dates';
 
 export default class ExpenseForm extends React.Component {
 
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            description: props.expense ? props.expense.description : '',
-            note: props.expense ? props.expense.note : '',
-            amount: props.expense ? (props.expense.amount / 100).toString() : '',
-            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
-            calendarFocused: false,
-            error: ''
-        }
-    }
+		this.state = {
+			description: props.expense ? props.expense.description : '',
+			note: props.expense ? props.expense.note : '',
+			amount: props.expense ? (props.expense.amount / 100).toString() : '',
+			createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+			calendarFocused: false,
+			error: ''
+		}
+	}
 
-    onDescriptionChange = (e) => {
-        const description = e.target.value;
-        this.setState( () => ({ description }) );
-    }
+	onDescriptionChange = (e) => {
+		const description = e.target.value;
+		this.setState(() => ({ description }));
+	}
 
-    onNoteChange = (e) => {
-        const note = e.target.value;
-        this.setState( () => ({ note }) );
-    }
+	onNoteChange = (e) => {
+		const note = e.target.value;
+		this.setState(() => ({ note }));
+	}
 
-    onAmountChange = (e) => {
-        const amount = e.target.value;
-        if(!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)){
-            this.setState( () => ({ amount }) );
-        }
-    }
+	onAmountChange = (e) => {
+		const amount = e.target.value;
+		if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
+			this.setState(() => ({ amount }));
+		}
+	}
 
-    onDateChange = (createdAt) => {
-        if(createdAt){
-            this.setState(() => ({ createdAt }))
-        } 
-    }
+	onDateChange = (createdAt) => {
+		if (createdAt) {
+			this.setState(() => ({ createdAt }))
+		}
+	}
 
-    onFocusChange = ( { focused } ) => {
-        this.setState( ()=> ({ calendarFocused: focused }))
-    }
+	onFocusChange = ({ focused }) => {
+		this.setState(() => ({ calendarFocused: focused }))
+	}
 
-    onSubmit = (e) => {
-        e.preventDefault();
+	onSubmit = (e) => {
+		e.preventDefault();
 
-        if(!this.state.description || !this.state.amount){
-            this.setState( () => ({ error: "Please provide description and amount" }) );
-        }else{
-            this.setState( () => ({ error: "" }) )
-            this.props.onSubmit({
-                description: this.state.description,
-                amount: parseFloat(this.state.amount, 10) * 100, // because we are working in cents.
-                createdAt: this.state.createdAt.valueOf(),
-                note: this.state.note
-            })
-        }
-    }
+		if (!this.state.description || !this.state.amount) {
+			this.setState(() => ({ error: "Please provide description and amount" }));
+		} else {
+			this.setState(() => ({ error: "" }))
+			this.props.onSubmit({
+				description: this.state.description,
+				amount: parseFloat(this.state.amount, 10) * 100, // because we are working in cents.
+				createdAt: this.state.createdAt.valueOf(),
+				note: this.state.note
+			})
+		}
+	}
 
-    render() {
-        return (
-            <div>
-                { this.state.error && <p>{ this.state.error }</p> }
-                <form onSubmit={this.onSubmit}>
-                    <input 
-                        type="text" 
-                        placeholder="Description"
-                        value={ this.state.description } 
-                        onChange={ this.onDescriptionChange } 
-                        autoFocus 
-                    />
-                    <input 
-                        type="text" 
-                        placeholder="Amount"
-                        value={ this.state.amount }
-                        onChange={ this.onAmountChange }
-                    />
-                    <SingleDatePicker
-                        date={ this.state.createdAt }
-                        onDateChange={ this.onDateChange }
-                        focused={ this.state.calendarFocused }
-                        onFocusChange={ this.onFocusChange }
-                        numberOfMonths={1}
-                        isOutsideRange={ () => false }
-                    />
+	render() {
+		return (
+			<form className="form" onSubmit={this.onSubmit}>
+				{this.state.error && <p className="form__error">{this.state.error}</p>}
+				<input
+					type="text"
+					className="text-input"
+					placeholder="Description"
+					value={this.state.description}
+					onChange={this.onDescriptionChange}
+					autoFocus
+				/>
+				<input
+					type="text"
+					className="text-input"
+					placeholder="Amount"
+					value={this.state.amount}
+					onChange={this.onAmountChange}
+				/>
+				<SingleDatePicker
+					date={this.state.createdAt}
+					onDateChange={this.onDateChange}
+					focused={this.state.calendarFocused}
+					onFocusChange={this.onFocusChange}
+					numberOfMonths={1}
+					isOutsideRange={() => false}
+				/>
 
-                    <textarea
-                        placeholder="Add a note for your expense (optional)" 
-                        onChange={ this.onNoteChange }
-                        value={ this.state.note }
-                    />
-                    <button>Add Expense</button>
-                </form>
-            </div>
-        )
-    }
+				<textarea
+					placeholder="Add a note for your expense (optional)"
+					className="text-area"
+					onChange={this.onNoteChange}
+					value={this.state.note}
+				/>
+				<div>
+					<button className="button">Save Expense</button>
+				</div>
+			</form>
+		)
+	}
 }
--- expensify-app/src/components/ExpenseListFilters.js
+++ expensify-app/src/components/ExpenseListFilters.js
@@ -4,70 +4,71 @@ import { DateRangePicker } from 'react-dates';
 import { setTextFilter, sortbyDate, sortbyAmount, setStartDate, setEndDate } from '../actions/filters';
 
 export class ExpenseListFilters extends React.Component {
-    state = {
-        calendarFocused: null
-    }
+	state = {
+		calendarFocused: null
+	}
 
-    onDatesChange = ({ startDate, endDate }) => {
-        this.props.setStartDate(startDate);
-        this.props.setEndDate(endDate);
-    }
+	onDatesChange = ({ startDate, endDate }) => {
+		this.props.setStartDate(startDate);
+		this.props.setEndDate(endDate);
+	}
 
-    onFocusChange = (calendarFocused) => {
-        this.setState(() => ({ calendarFocused }))
-    }
+	onFocusChange = (calendarFocused) => {
+		this.setState(() => ({ calendarFocused }))
+	}
 
-    onTextChange = (e) => {
-        this.props.setTextFilter(e.target.value);
-    }
+	onTextChange = (e) => {
+		this.props.setTextFilter(e.target.value);
+	}
 
-    onSortChange = (e) => {
-        e.target.value === "amount" ?  this.props.sortByAmount() : this.props.sortByDate();
-    }
+	onSortChange = (e) => {
+		e.target.value === "amount" ? this.props.sortByAmount() : this.props.sortByDate();
+	}
 
-    render() {
-        return (
-            <div>
-                <input 
-                    type="text" 
-                    value={this.props.filters.text} 
-                    onChange={ this.onTextChange } 
-                />
-                <select 
-                    value={ this.props.filters.sortBy }
-                    onChange={ this.onSortChange }
-                >
-                    <option value="date">Date</option>
-                    <option value="amount">Amount</option>
-                </select>
-                <DateRangePicker
-                    startDate={ this.props.filters.startDate }
-                    endDate={ this.props.filters.endDate }
-                    onDatesChange={ this.onDatesChange }
-                    focusedInput={ this.state.calendarFocused }
-                    onFocusChange={ this.onFocusChange }
-                    numberOfMonths={1}
-                    isOutsideRange={ () => false } //choose dates from past
-                    showClearDates={ true }
-                />
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="content-container">
+				<div className="input-group">
+					<div className="input-group__item">
+						<input type="text" className="text-input" placeholder="Search Expenses" value={this.props.filters.text} onChange={this.onTextChange} />
+					</div>
+					<div className="input-group__item">
+						<select className="select" value={this.props.filters.sortBy} onChange={this.onSortChange}>
+							<option value="date">Date</option>
+							<option value="amount">Amount</option>
+						</select>
+					</div>
+					<div className="input-group__item">
+						<DateRangePicker
+							startDate={this.props.filters.startDate}
+							endDate={this.props.filters.endDate}
+							onDatesChange={this.onDatesChange}
+							focusedInput={this.state.calendarFocused}
+							onFocusChange={this.onFocusChange}
+							numberOfMonths={1}
+							isOutsideRange={() => false} //choose dates from past
+							showClearDates={true}
+						/>
+					</div>
+				</div>
+			</div>
+		)
+	}
 
 }
 
 const mapStateToProps = (state) => {
-    return {
-        filters: state.filters
-    }
+	return {
+		filters: state.filters
+	}
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    setTextFilter: (text) => dispatch(setTextFilter(text)),
-    sortByDate: () => dispatch(sortByDate()),
-    sortByAmount: () => dispatch(sortByAmount()),
-    setStartDate: (startDate) => dispatch(setStartDate(startDate)),
-    setEndDate: (endDate) => dispatch(setEndDate(endDate))
+	setTextFilter: (text) => dispatch(setTextFilter(text)),
+	sortByDate: () => dispatch(sortByDate()),
+	sortByAmount: () => dispatch(sortByAmount()),
+	setStartDate: (startDate) => dispatch(setStartDate(startDate)),
+	setEndDate: (endDate) => dispatch(setEndDate(endDate))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
